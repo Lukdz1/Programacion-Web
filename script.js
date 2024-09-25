e9d003c66ccf43d32c1d5289a901f1395962a7e3
Lucero Hernández Hernández <160188522+Lukdz1@users.noreply.github.com>
2024-09-24 18:01:57 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-alert("hola, c5IV7 desde eñl js externo");
+//CLASE 24/09/24
+//NOMBRE: Hernández Hernández Lucero
+//NOMBRE: Rodriguez Tecpanecatl Valentina Xanaeth
+
+/* LAS VARIABLES GUARDAN INFORMACIÓN QUE PUEDES REUTILIZAR */
+
+//DECLARACIÓN DE VARIABLES 
+let nombre = 'Pedro'; // usa let declara variables que pueden cambiar su valor
+const PI = 3.1416;
+var edad = 25;
+
+//operadores
+let a = 5;
+let b = 2;
+let suma = a + b; //7
+let resta = a - b; //3
+let multiplicacion = a * b; //10
+let división = a / b; //2.5
+
+//operadores de comparación
+a > b; //true
+a < b; //false
+a == b; //false
+a === b; //false (compara valor y tipo)
+
